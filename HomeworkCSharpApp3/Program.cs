10383b2203493e51b2283da9e73fbc3667b8667f
Ivan <IvanBWork@yandex.ru>
2024-09-02 22:29:18 +0300

    Добавьте файлы проекта.

---
+++ HomeworkCSharpApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace HomeworkCSharpApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                ChatServer.Server();
+            }
+            else
+            {
+                ChatClient.Client(args);
+            }
+        }
+    }
+}
